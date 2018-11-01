6b72e4d7bec1adfe2bd8c48948345870c26ef1d2
Jia Xu <xj.joye@outlook.com>
2018-11-01 04:51:12 -0700

    add Cylinder file complete

--- Cylinder.cpp
+++ Cylinder.cpp
@@ -1,26 +1,43 @@
 #include <iostream>
 using namespace std;
 #define PI 3.1415926
+
 class Cylinder{
 	private:
-		float radius,high;
+		int radius;
+		int high;
 	public:
-		Cylinder(float r,float h):
-		radius(r),high(h){};
-		float GetAtee();
-		float GetVolume();
+		void setCylinder(int r,int h);
+		int GetArea();
+		int GetVol();
 	};
 
-float Cylinder::GetAtee(){
+void Cylinder::setCylinder(int r_input, int h_input){
+	radius = r_input;
+	high = h_input;
+}
+
+int Cylinder::GetArea(){
 	return PI*radius*2*high + PI*radius*radius*2;
 }
 
-float Cylinder::GetVolume(){
+int Cylinder::GetVol(){
 	return PI*radius*radius*high;
 }
 
-void main(){
-	Cylinder cy(1,1);
-	cout<<cy.GetAtee()<<endl;
-	cout<<cy.GetVolume()<<endl;
+int main(){
+	//get user input r and h
+	int r, h;
+	cout << "Enter an integer for r: \n";
+	cin >> r;
+	cout << "The r is " << r << " Now Enter an integer for h: \n";
+	cin >> h;
+	cout << "h is " << h << endl;
+
+	Cylinder cy; 
+	cy.setCylinder(r, h);
+	cout<<cy.GetArea()<<endl;
+	cout<<cy.GetVol()<<endl;
+	
+	return 0;
 }
